2feac819f10ddc9cd9b2db58400e1c036128c9ef
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-29 12:25:13 +0300

    addde some consts

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ let strict = false;
 let noise = true;
 let on = false;
 let win;
+
+// ---- const
+const turnCounter = document.getElementById("turn");
+const topLeft = document.getElementById("topleft");
+const topRight = document.getElementById("topright");
+const bottomLeft = document.getElementById("bottomleft");
+const bottomRight = document.getElementById("bottomright");
+const strictButton = document.getElementById("strict");
+const onButton = document.getElementById("on");
+const startButton = document.getElementById("start");
