4d187f0c239e6e25bc7e22873782c90808ea167e
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-29 13:11:43 +0300

    added one, two, three, four functions

--- app.js
+++ app.js
@@ -87,3 +87,43 @@ function gameTurn() {
 		}, 200);
 	}
 }
+
+function one() {
+	if (noise) {
+		let audio = document.getElementById("clip1");
+		audio.play();
+	}
+
+	noise = true;
+	topLeft.style.backgroundColor = "lightgreen";
+}
+
+function two() {
+	if (noise) {
+		let audio = document.getElementById("clip2");
+		audio.play();
+	}
+
+	noise = true;
+	topLeft.style.backgroundColor = "tomato";
+}
+
+function three() {
+	if (noise) {
+		let audio = document.getElementById("clip3");
+		audio.play();
+	}
+
+	noise = true;
+	topLeft.style.backgroundColor = "yellow";
+}
+
+function four() {
+	if (noise) {
+		let audio = document.getElementById("clip4");
+		audio.play();
+	}
+
+	noise = true;
+	topLeft.style.backgroundColor = "lightskyblue";
+}
