527806cd924527750ade12ba5c9ca72dc68c320c
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-29 12:16:36 +0300

    added some variables

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// ---- variables
+let order = [];
+let playerOrder = [];
+let flash;
+let tern;
+let good;
+let compTern;
+let intervalID;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
